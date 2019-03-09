b23ffc8d0419e01c331f1912da2f593beaf9ae29
eugene afanasovich <eugeneafanasovich@gmail.com>
2019-03-09 13:34:17 +0300

    Add project files.

---
+++ ET.ClientConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ET.ClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ET.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ET.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SystemConsole.WriteLine("Hello World!");
+        }
+    }
+}
