3d206a52bb45d256076cafb7965443d553f60b79
eugene afanasovich <eugeneafanasovich@gmail.com>
2019-04-04 10:47:08 +0300

    statistics

---
+++ ClientAndroid/StatisticsActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ClientAndroid
+{
+    [Activity(Label = "StatisticsAvtivity")]
+    public class StatisticsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_statistics);
+            // Create your application here
+        }
+    }
+}
