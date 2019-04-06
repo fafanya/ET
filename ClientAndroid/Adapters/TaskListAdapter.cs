17083375921aa366932c1fdaf9e2ba0d0f43b06d
eugene afanasovich <eugeneafanasovich@gmail.com>
2019-04-06 10:50:41 +0300

    adapters

---
+++ ClientAndroid/Adapters/TaskListAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using ClientCommon;
+
+namespace ClientAndroid
+{
+    public class TaskListAdapter : BaseAdapter<Task>
+    {
+        private Task[] m_Tasks;
+        private Activity m_Context;
+
+        public TaskListAdapter(Activity context, Task[] tasks) : base()
+        {
+            m_Context = context;
+            m_Tasks = tasks;
+        }
+
+        public override Task this[int position]
+        {
+            get
+            {
+                return m_Tasks[position];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return m_Tasks.Length;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return m_Tasks[position].TaskId;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClientAndroid/Adapters/TestListAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using ClientCommon;
+
+namespace ClientAndroid
+{
+    public class TestListAdapter : BaseAdapter<Test>
+    {
+        private Test[] m_Tests;
+        private Activity m_Context;
+
+        public TestListAdapter(Activity context, Test[] tests) : base()
+        {
+            m_Context = context;
+            m_Tests = tests;
+        }
+
+        public override Test this[int position]
+        {
+            get
+            {
+                return m_Tests[position];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return m_Tests.Length;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return m_Tests[position].TestId;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
